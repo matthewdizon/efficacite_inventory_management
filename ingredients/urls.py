9020cdbef56dcd0667a7025a14dce2d466b563d9
Matthew Dominic Dizon <matthewdominicdizon@Matthews-MacBook-Air.local>
2022-03-20 17:58:46 +0800

    created add ingredient button and functionality

--- ingredients/urls.py
+++ ingredients/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # Path for Accounts Model
     path('', views.home, name='ingredient_index'),
-    # path('add/', views.add_product, name='add_product'),
+    path('add/', views.add_ingredient, name='add_ingredient'),
     path('<int:pk>/', views.view_ingredient, name='view_ingredient'),
     # path('update/<int:pk>', views.update_product, name='update_product'),
     # path('delete/<int:pk>', views.delete_product, name='delete_product')
--- ingredients/views.py
+++ ingredients/views.py
@@ -6,6 +6,44 @@ def home(request):
     context = {"ingredients":ingredients}
     return render(request, 'ingredients/index.html', context)
 
+def add_ingredient(request):
+    if(request.method=="POST"):
+        ingredient_name = request.POST.get('ingredient_name')
+        description = request.POST.get('description')
+        current_quantity = request.POST.get('current_quantity')
+        quantity_threshold = request.POST.get('quantity_threshold')
+        metric = request.POST.get('metric')
+        created_at = request.POST.get('date')
+        # ingredients = request.POST.get('ingredients')
+        # print(ingredients)
+        
+        try:
+            # ingredients = Ingredient.objects.filter(pk=ingredients)
+            
+            instance = Ingredient.objects.create(
+                name=ingredient_name,
+                description=description,
+                current_quantity=current_quantity,
+                quantity_threshold=quantity_threshold,
+                metric=metric,
+                created_at=created_at,
+            )
+            
+            # instance.ingredients.add(ingredients)
+
+            # message="Product created successfully"
+            # return redirect(f"/foods?message={message}")
+            return redirect(f"/inventory")
+
+        except Exception as e:
+            message = e
+            # context = {'ingredients':ingredients, 'message':message}
+            # print(context)
+            return render(request, 'ingredients/add_ingredient.html')
+
+    else:
+        return render(request, 'ingredients/add_ingredient.html')
+
 def view_ingredient(request, pk):
     ingredient = get_object_or_404(Ingredient, pk=pk)
     if request.GET.get('message') == None:
