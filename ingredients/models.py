e1510216e55a82208fb7474daa127fc2271bd716
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-04-13 01:46:27 +0800

    added selecting supplier in adding ingredient and quantity/expiration priority

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from suppliers.models import Supplier
+from django.utils.timezone import now
 
 METRIC_CHOICES = (
     ('kg','kilogram'),
@@ -14,6 +16,11 @@ class Ingredient(models.Model):
     quantity_threshold = models.FloatField(max_length=250)
     metric = models.CharField(max_length=25, choices=METRIC_CHOICES, default='kg')
     created_at = models.DateTimeField(auto_now_add=True)
+    expiration_date = models.DateTimeField(default=now)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"{self.pk}: {self.name} created at: {self.created_at}"
+
+    def get_quantity_ratio(self):
+        return self.current_quantity / self.quantity_threshold
--- ingredients/views.py
+++ ingredients/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
+from suppliers.models import Supplier
 
 def home(request):
     ingredients = Ingredient.objects.all()
+    print(ingredients[0].current_quantity/ingredients[0].quantity_threshold)
     context = {"ingredients":ingredients}
     return render(request, 'ingredients/index.html', context)
 
 def add_ingredient(request):
+    supplier_objects = Supplier.objects.all()
+    context = {'suppliers':supplier_objects}
+
     if(request.method=="POST"):
         ingredient_name = request.POST.get('ingredient_name')
         description = request.POST.get('description')
@@ -14,12 +19,17 @@ def add_ingredient(request):
         quantity_threshold = request.POST.get('quantity_threshold')
         metric = request.POST.get('metric')
         created_at = request.POST.get('date')
-        # ingredients = request.POST.get('ingredients')
-        # print(ingredients)
+        supplier_id = request.POST.get('supplier')
+
+        print(supplier_id)
+
+        supplier = Supplier.objects.filter(pk=supplier_id)
+        print(supplier)
+        
+        # suppliers = Ingredient.objects.filter(pk__in=supplier_ids)
+        # print(suppliers)
         
         try:
-            # ingredients = Ingredient.objects.filter(pk=ingredients)
-            
             instance = Ingredient.objects.create(
                 name=ingredient_name,
                 description=description,
@@ -27,22 +37,21 @@ def add_ingredient(request):
                 quantity_threshold=quantity_threshold,
                 metric=metric,
                 created_at=created_at,
+                supplier=supplier[0],
             )
-            
-            # instance.ingredients.add(ingredients)
 
-            # message="Product created successfully"
-            # return redirect(f"/foods?message={message}")
+            # instance.suppliers.add(*suppliers)
+            
             return redirect(f"/inventory")
 
         except Exception as e:
             message = e
             # context = {'ingredients':ingredients, 'message':message}
             print(message)
-            return render(request, 'ingredients/add_ingredient.html')
+            return render(request, 'ingredients/add_ingredient.html', context)
 
     else:
-        return render(request, 'ingredients/add_ingredient.html')
+        return render(request, 'ingredients/add_ingredient.html', context)
 
 def view_ingredient(request, pk):
     ingredient = get_object_or_404(Ingredient, pk=pk)
