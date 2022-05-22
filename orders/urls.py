db8ecc4b0fcffc9fc2c549c97ddd13dfbdf2ca4a
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-05-22 17:59:53 +0800

    multiple orders in one request

--- orders/urls.py
+++ orders/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='order_index'),
-    path('add/', views.add_order, name='add_order'),
+    path('add/', views.OrderAddView.as_view(), name='add_order'),
     path('update/<int:pk>', views.update_order, name='update_order'),
     path('delete/<int:pk>', views.delete_order, name='delete_order'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from suppliers.models import Supplier
-from ingredients.models import Ingredient
+# from ingredients.models import Ingredient
+from products.models import Product
 from django.contrib import messages
 
 def home(request):
@@ -13,25 +14,38 @@ def home(request):
 def add_order(request):
     if(request.method=="POST"):
         order_objects = Order.objects.all()
+        # try:
+        #     product = Product.objects.get(name=request.POST.get('o_food'))
+        #     qty = request.POST.get('o_quantity')
+        #     payment = request.POST.get('o_pmode')
+        #     # orderedat = request.POST.get('o_orderedat')
+        #     status = request.POST.get('o_status')
+        #     Order.objects.create(ingredient_order= ingredient, qty=qty, payment_mode=payment, status=status)
+        #     new_qty = int(ingredient.current_quantity) - int(qty)
+        #     product.current_quantity = new_qty
+        #     product.save()
+        #     return redirect(f"/orders")
         try:
-            ingredient = Ingredient.objects.get(name=request.POST.get('o_food'))
+            product = Product.objects.get(name=request.POST.get('o_food'))
             qty = request.POST.get('o_quantity')
             payment = request.POST.get('o_pmode')
             # orderedat = request.POST.get('o_orderedat')
             status = request.POST.get('o_status')
-            Order.objects.create(ingredient_order= ingredient, qty=qty, payment_mode=payment, status=status)
-            new_qty = int(ingredient.current_quantity) - int(qty)
-            ingredient.current_quantity = new_qty
-            ingredient.save()
+            print(product.getName())
+            # print(product.ingredients.all())
+            for i in product.ingredients.all():
+                print(i.name, i.current_quantity)
+            # product.current_quantity = new_qty
+            # product.save()
             return redirect(f"/orders")
         except:
             messages.error(request, 'Please complete Order details!')
-            i = Ingredient.objects.all()
-            return render(request, 'orders/add_order.html', {'ingredients':i})
+            i = Product.objects.all()
+            return render(request, 'orders/add_order.html', {'products':i})
     
     else:
-        i = Ingredient.objects.all()
-        return render(request, 'orders/add_order.html', {'ingredients':i})
+        i = Product.objects.all()
+        return render(request, 'orders/add_order.html', {'products':i})
 
 def update_order(request, pk):
     o = get_object_or_404(Order, pk=pk)
@@ -51,3 +65,38 @@ def delete_order(request, pk):
     order = Order.objects.get(pk=pk)
     order.delete()
     return redirect(f"/orders")
+
+from django.views.generic import ListView, TemplateView
+from .forms import OrderFormSet
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+
+class OrderAddView(TemplateView):
+    template_name = "orders/add_order.html"
+
+    def get(self, *args, **kwargs):
+        formset = OrderFormSet(queryset=Order.objects.none())
+        return self.render_to_response({'order_formset': formset})
+
+    # Define method to handle POST request
+    def post(self, *args, **kwargs):
+
+        formset = OrderFormSet(data=self.request.POST)
+
+        # Check if submitted forms are valid
+        if formset.is_valid():
+            for f in formset:
+                cd = f.cleaned_data
+                qty = cd['qty']
+                print("cd", cd)
+                print("qty", qty)
+                ingredient = cd['ingredient_order']
+                print("ingredient", ingredient)
+                new_qty = int(ingredient.current_quantity) - int(qty)
+                ingredient.current_quantity = new_qty
+                ingredient.save()
+
+            formset.save()
+            return redirect(reverse_lazy("add_order"))
+
+        return self.render_to_response({'order_formset': formset})
