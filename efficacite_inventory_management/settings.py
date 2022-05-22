d0191ed3717b6dc598cfe038385bce851221fee0
Jacob <jacob.dygico@obf.ateneo.edu>
2022-05-22 23:44:11 +0800

    added export sales report function in home page

--- efficacite_inventory_management/settings.py
+++ efficacite_inventory_management/settings.py
@@ -131,6 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = "grillhouseapp@gmail.com"
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_PASSWORD = "Admin12345*"
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('create_account', views.create_account, name='create_account'),
     path('home', views.home, name='index'),
+    path('export_salesreport', views.export_salesreport, name='export_salesreport'),
 ]
