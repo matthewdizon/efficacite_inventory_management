7b49bfb34c1ef752072e58f8f76356910c822d2d
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-05-23 00:33:45 +0800

    test fix for heroku static files

--- efficacite_inventory_management/settings.py
+++ efficacite_inventory_management/settings.py
@@ -128,8 +128,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
